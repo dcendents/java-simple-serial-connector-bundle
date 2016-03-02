ca69fa376fdaa336bf525a4b4b582d186acd08ce
Daniel Beland <dcendents@gmail.com>
2016-03-02 09:11:45 -0500

    Downgraded the osgi core library to the oldest version possible
    Downgraded asm and fixed the static block remover (downgraded asm because I needed EmptyVisitor as per the stack overflow post I found)

--- src/main/java/jssc/osgi/StaticCodeBlockRemover.java
+++ src/main/java/jssc/osgi/StaticCodeBlockRemover.java
@@ -4,50 +4,42 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.commons.EmptyVisitor;
 import org.osgi.framework.hooks.weaving.WeavingHook;
 import org.osgi.framework.hooks.weaving.WovenClass;
 
-import static org.objectweb.asm.Opcodes.*;
 
+import org.objectweb.asm.ClassAdapter;
 
 public class StaticCodeBlockRemover implements WeavingHook {
-    
+
 	public void weave(WovenClass wovenClass) {
-		if(wovenClass.getClassName().equals("jssc.SerialNativeInterface"))
-		{
+		if (wovenClass.getClassName().equals("jssc.SerialNativeInterface")) {
 			wovenClass.setBytes(transform(wovenClass.getBytes()));
 		}
 	}
 
-    public byte[] transform(byte[] origClassData) {
-        ClassReader cr = new ClassReader(origClassData);
-        final ClassWriter cw = new ClassWriter(cr, Opcodes.ASM4);
-        
-        // wrap the ClassWriter with a ClassVisitor that adds the static block to
-        // initialize the above fields
-        ClassVisitor cv = new ClassVisitor(ASM4, cw) {
+	public byte[] transform(byte[] origClassData) {
+		ClassReader cr = new ClassReader(origClassData);
+		final ClassWriter cw = new ClassWriter(0);
 
-          public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-            if (cv == null) {
-              return null;
-            }
-            MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
-            if ("<clinit>".equals(name)) {
-              return null;
-            } else {
-              return mv;
-            }
-          }
-        };
+		// feed the original class to the ClassAdapter
+		cr.accept(new ClinitRemoverClassAdapter(cw), 0);
 
-        // feed the original class to the wrapped ClassVisitor
-        cr.accept(cv, 0);
+		// produce the modified class
+		return cw.toByteArray();
+	}
 
-        // produce the modified class
-        byte[] newClassData = cw.toByteArray();
-        return newClassData;
-      }
+	class ClinitRemoverClassAdapter extends ClassAdapter {
+	    public ClinitRemoverClassAdapter(final ClassVisitor cv) {
+	        super(cv);
+	    }
 
+	    public MethodVisitor visitMethod(final int access, final String name, final String desc, final String signature, final String[] exceptions) {
+	        if (name.equals("<clinit>")) {
+	            return new EmptyVisitor();
+	        }
+	        return cv.visitMethod(access, name, desc, signature, exceptions);
+	    }
+	}
 }
-
