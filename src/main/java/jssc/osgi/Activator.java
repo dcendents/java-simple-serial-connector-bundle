f7a4828a34ce8141dc793763704a6cd7a2e3fde9
Daniel Beland <dcendents@gmail.com>
2016-03-14 16:25:22 -0400

    Set the value of osType, which was done in the static initializer we removed

--- src/main/java/jssc/osgi/Activator.java
+++ src/main/java/jssc/osgi/Activator.java
@@ -7,20 +7,20 @@ import org.osgi.framework.ServiceRegistration;
 import org.osgi.framework.hooks.weaving.WeavingHook;
 
 public class Activator implements BundleActivator {
-	
-	private ServiceRegistration staticCodeRemoverHook;
-	
+
+	private ServiceRegistration<?> staticCodeRemoverHook;
+
 	public void start(BundleContext context) throws Exception {
 		String processor = context.getProperty(Constants.FRAMEWORK_PROCESSOR);
 		processor = processor.replace('-', '_');
 		System.loadLibrary("jSSC-2.8_" + processor);
 		staticCodeRemoverHook = context.registerService(WeavingHook.class.getName(), new StaticCodeBlockRemover(), null);
-		
+
 	}
-	
+
 	public void stop(BundleContext context) throws Exception {
 		staticCodeRemoverHook.unregister();
-		
+
 	}
 
 }
--- src/main/java/jssc/osgi/StaticCodeBlockRemover.java
+++ src/main/java/jssc/osgi/StaticCodeBlockRemover.java
@@ -3,11 +3,11 @@ package jssc.osgi;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.commons.EmptyVisitor;
 import org.osgi.framework.hooks.weaving.WeavingHook;
 import org.osgi.framework.hooks.weaving.WovenClass;
-
 
 import org.objectweb.asm.ClassAdapter;
 
@@ -31,15 +31,42 @@ public class StaticCodeBlockRemover implements WeavingHook {
 	}
 
 	class ClinitRemoverClassAdapter extends ClassAdapter {
-	    public ClinitRemoverClassAdapter(final ClassVisitor cv) {
-	        super(cv);
-	    }
+		public ClinitRemoverClassAdapter(final ClassVisitor cv) {
+			super(cv);
+		}
 
-	    public MethodVisitor visitMethod(final int access, final String name, final String desc, final String signature, final String[] exceptions) {
-	        if (name.equals("<clinit>")) {
-	            return new EmptyVisitor();
-	        }
-	        return cv.visitMethod(access, name, desc, signature, exceptions);
-	    }
+		@Override
+		public MethodVisitor visitMethod(final int access, final String name, final String desc, final String signature, final String[] exceptions) {
+			if ("<clinit>".equals(name)) {
+				return new EmptyVisitor();
+			}
+			return cv.visitMethod(access, name, desc, signature, exceptions);
+		}
+
+		@Override
+		public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
+			if ("osType".equals(name)) {
+				final int OS_LINUX = 0;
+				final int OS_WINDOWS = 1;
+				final int OS_SOLARIS = 2;
+				final int OS_MAC_OS_X = 3;
+
+				int val = -1;
+				String osName = System.getProperty("os.name");
+				if (osName.equals("Linux")) {
+					val = OS_LINUX;
+				} else if (osName.startsWith("Win")) {
+					val = OS_WINDOWS;
+				} else if (osName.equals("SunOS")) {
+					val = OS_SOLARIS;
+				} else if (osName.equals("Mac OS X") || osName.equals("Darwin")) {
+					val = OS_MAC_OS_X;
+				}
+
+				return super.visitField(access, name, desc, signature, val);
+			}
+
+			return super.visitField(access, name, desc, signature, value);
+		}
 	}
 }
