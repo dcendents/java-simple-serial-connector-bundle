a83da13402e988fa4af609cc69b2140bfa3d2589
Daniel Beland <dcendents@gmail.com>
2016-04-11 16:32:38 -0400

    Fixing bundle to work on ARM processors
    (Tested only with a BeagleBone Black)

--- src/main/java/jssc/osgi/Activator.java
+++ src/main/java/jssc/osgi/Activator.java
@@ -1,5 +1,8 @@
 package jssc.osgi;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
@@ -12,7 +15,39 @@ public class Activator implements BundleActivator {
 
 	public void start(BundleContext context) throws Exception {
 		String processor = context.getProperty(Constants.FRAMEWORK_PROCESSOR);
-		processor = processor.replace('-', '_');
+
+        if("arm".equals(processor)) {
+            String floatStr = "sf";
+            String javaLibPath = System.getProperty("java.library.path");
+            String abi = System.getProperty("sun.arch.abi");
+
+            boolean javaLibHF = javaLibPath != null && (javaLibPath.toLowerCase().contains("gnueabihf") || javaLibPath.toLowerCase().contains("armhf"));
+            boolean abiHF = abi != null && (abi.toLowerCase().contains("gnueabihf") || abi.toLowerCase().contains("armhf"));
+
+            if(javaLibHF || abiHF){
+                floatStr = "hf";
+            }
+            else {
+                try {
+                    Process readelfProcess =  Runtime.getRuntime().exec("readelf -A /proc/self/exe");
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(readelfProcess.getInputStream()));
+                    String buffer = "";
+                    while((buffer = reader.readLine()) != null && !buffer.isEmpty()){
+                        if(buffer.toLowerCase().contains("Tag_ABI_VFP_args".toLowerCase())){
+                            floatStr = "hf";
+                            break;
+                        }
+                    }
+                    reader.close();
+                }
+                catch (Exception ex) {
+                    //Do nothing
+                }
+            }
+            processor = processor + floatStr;
+        }
+
+        processor = processor.replace('-', '_');
 		System.loadLibrary("jSSC-2.8_" + processor);
 		staticCodeRemoverHook = context.registerService(WeavingHook.class.getName(), new StaticCodeBlockRemover(), null);
 
